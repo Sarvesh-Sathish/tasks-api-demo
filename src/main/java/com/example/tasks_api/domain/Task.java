8a28c4f99ce37c5420c42b0b5b2bae06fa4ba9c7
Sarvesh Sathish <sarveshsathish.2004@gmail.com>
2025-08-16 17:16:41 -0400

    Implemented Task Doc

--- src/main/java/com/example/tasks_api/domain/Task.java
+++ src/main/java/com/example/tasks_api/domain/Task.java
@@ -1,4 +1,19 @@
 package com.example.tasks_api.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.time.Instant;
+
+@Document(collection = "tasks")
 public class Task {
+    @Id
+    private String id;
+
+    private String title;
+    private String description;
+    private boolean done;
+    private Instant createdAt;
+
 }
